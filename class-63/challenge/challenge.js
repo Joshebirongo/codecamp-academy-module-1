7b30fdb4b45783951cde9930879422324a1b25ab
Jonathan Shebirongo <jonathanshebirongo07@gmail.com>
2026-04-14 17:53:02 +0200

    fix class-63

---
+++ class-63/challenge/challenge.js
@@ -0,0 +1,76 @@
+// Author Muliri Shebirongo Jonathan <Joshebirongo>
+
+// Countdown from 10 to 1
+for (let x = 10; x >= 1; x--) {
+  // Result
+
+  console.log(x);
+}
+console.log("Lift off!");
+
+/*
+    DESK TOP TEST
+    countdown (Decrease form 10 to 1)
+    -----------------------------------------------
+    Iteration 1
+    x = 10
+    decrease = x - 0
+    Output = 10
+    -----------------------------------------------
+    Iteration 2
+    x = 9
+    decrease = x - 1
+    Output = 9
+    -----------------------------------------------
+    Iteration 3
+    x = 8
+    decrease = x - 1
+    Output = 8
+    -----------------------------------------------
+    Iteration 4
+    x = 8
+    decrease = x - 1
+    Output = 7
+    -----------------------------------------------
+    Iteration 5
+    x = 7
+    decrease = x - 1
+    Output = 6
+    -----------------------------------------------
+    Iteration 6
+    x = 7
+    decrease = x - 1
+    Output = 6
+    -----------------------------------------------
+    Iteration 7
+    x = 6
+    decrease = x - 1
+    Output = 5
+    -----------------------------------------------
+    Iteration 8
+    x = 5
+    decrease = x - 1
+    Output = 4
+    -----------------------------------------------
+    Iteration 9
+    x = 4
+    decrease = x - 1
+    Output = 3
+    -----------------------------------------------
+    Iteration 10
+    x = 3
+    decrease = x - 1
+    Output = 2
+    -----------------------------------------------
+    Iteration 11
+    x = 2
+    decrease = x - 1
+    Output = 1
+
+    Loop stops.
+    -----------------------------------------------
+    Next iteration 
+    x = 1
+    decrease = x-1 is < 1 (Condition x must be >= 1)
+    Output : Lift off!
+*/
--- class-63/challenge/problem-32/problem-32.js
+++
@@ -1,9 +0,0 @@
-// Author Muliri Shebirongo Jonathan <Joshebirongo>
-
-// Countdown from 10 to 1
-for (let x = 10; x >= 1; x--) {
-  // Result
-
-  console.log(x);
-}
-console.log("Lift off!");
