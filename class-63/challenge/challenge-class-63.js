fddc454e2ef6c36c19ceb5977c73af757c3fff61
Jonathan Shebirongo <jonathanshebirongo07@gmail.com>
2026-04-20 19:10:14 +0200

    fix class-63 and class-67

---
+++ class-63/challenge/challenge-class-63.js
@@ -0,0 +1,74 @@
+// Author : Muhindo Kotya Delphin
+
+// Loop from 10 to 1
+for (let x = 10; x >= 1; x--) {
+  //Display Result
+
+  console.log(x);
+}
+console.log("Lift off!");
+
+/*
+    DESK TOP TEST
+    countdown form 10 to 1
+    -----------------------------------------------
+    Iteration 1
+    x = 10
+    decrease = x - 0
+    Output = 10
+    -----------------------------------------------
+    Iteration 2
+    x = 9
+    decrease = x - 1
+    Output = 9
+    -----------------------------------------------
+    Iteration 3
+    x = 8
+    decrease = x - 1
+    Output = 8
+    -----------------------------------------------
+    Iteration 4
+    x = 8
+    decrease = x - 1
+    Output = 7
+    -----------------------------------------------
+    Iteration 5
+    x = 7
+    decrease = x - 1
+    Output = 6
+    -----------------------------------------------
+    Iteration 6
+    x = 7
+    decrease = x - 1
+    Output = 6
+    -----------------------------------------------
+    Iteration 7
+    x = 6
+    decrease = x - 1
+    Output = 5
+    -----------------------------------------------
+    Iteration 8
+    x = 5
+    decrease = x - 1
+    Output = 4
+    -----------------------------------------------
+    Iteration 9
+    x = 4
+    decrease = x - 1
+    Output = 3
+    -----------------------------------------------
+    Iteration 10
+    x = 3
+    decrease = x - 1
+    Output = 2
+    -----------------------------------------------
+    Iteration 11
+    x = 2
+    decrease = x - 1
+    Output = 1
+
+    Loop stops.
+    -----------------------------------------------
+    Next iteration 
+    Output : Lift off!
+*/
